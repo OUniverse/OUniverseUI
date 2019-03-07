32c416801f7811a1d32ae51e4a8f51a4db677021
Ceo <notanemail@sorry.com>
2019-03-07 16:11:26 -0500

    User Object creation in IO_User

--- io/user/user.js
+++ io/user/user.js
@@ -1,16 +1,21 @@
 class UO_USER
 {
-	constructor()
+	constructor(newUser)
 	{
-
+			this.id = newUser.ID
+			this.sn = newUser.SN
 	}
 }
 
 class IO_USER
 {
+
 	constructor()
-	{
+	{	
+		this.users = []
 		console.log("IO User:Active")
+		engine.on('user_sendUserData', this.registerUser.bind(this))
+		engine.on('user_modeSelect', this.modeSelect.bind(this))
 	}
 
 	start()
@@ -18,6 +23,19 @@ class IO_USER
 		
 	}
 
+	modeSelect()
+	{
+		for(var i=0;i<this.users.length;i++)
+		{
+			console.log(this.users[i].id)
+			console.log(this.users[i].sn)
+		}
+	}
+
+	registerUser(newUser)
+	{
+		this.users.push(new UO_USER(newUser))
+	}
 
 	end()
 	{
